bc736de20bf8748a44624e22f70c51146489bed0
Himm <yumm007@gmail.com>
2012-06-21 00:24:21 +0800

    java调用C

---
+++ java2c/HelloWorld.java
@@ -0,0 +1,17 @@
+class HelloWorld {
+
+    private native void print();
+
+    public static void main(String[] args) {
+
+        new HelloWorld().print();
+
+    }
+
+    static {
+
+        System.loadLibrary("HelloWorld");
+
+    }
+
+}
